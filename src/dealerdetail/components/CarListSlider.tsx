527ebb12583e224380feb581b23a157219c6645c
mna11 <thkim306@gmail.com>
2025-06-22 14:39:55 +0900

    Design: 딜러 상세페이지 디자인 수정  & 딜러 정기고사 성적 카운팅 애니메이션

--- src/dealerdetail/components/CarListSlider.tsx
+++ src/dealerdetail/components/CarListSlider.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './style.css';
 import CarCard from './CarCard';
 
@@ -15,12 +15,72 @@ interface CarListSliderProps {
 }
 
 const CarListSlider: React.FC<CarListSliderProps> = ({ carList, onClick }) => {
+
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const [isDrag, setIsDrag] = useState(false);
+  const [start, setStart] = useState(0);
+
+  const [startPageX, setStartPageX] = useState(0);
+  const [endPageX, setEndPageX] = useState(0);
+
+  const onMouseDown = (event) => {
+    event.preventDefault();
+    setIsDrag(true);
+    if (scrollRef.current) {
+      setStart(event.pageX + scrollRef.current.scrollLeft);
+      setStartPageX(event.pageX);
+    }
+  }
+
+  const onDragMove = (event) => {
+    if (isDrag) {
+        if (scrollRef.current) {
+            scrollRef.current.scrollLeft = start - event.pageX;
+        }
+    }
+  };
+
+  const onMouseUp = (event) => {
+    setEndPageX(event.pageX);
+    setIsDrag(false);
+  };
+
+  const handleClick = (carId: number) => {
+    if (startPageX - endPageX == 0) {
+      onClick(carId);
+    }
+  }
+  
+  useEffect(() => {
+    const container = scrollRef.current;
+    if (!container) return;
+
+    const handleWheel = (e: WheelEvent) => {
+      if (Math.abs(e.deltaY) > Math.abs(e.deltaX)) {
+        e.preventDefault();
+        container.scrollLeft += e.deltaY;
+      }
+    };
+
+    container.addEventListener('wheel', handleWheel, { passive: false });
+    
+    return () => {
+      container.removeEventListener('wheel', handleWheel);
+    };
+  }, []);
+  
   return (
     <div className="car-slider-wrapper">
-      <div className="car-list-slider">
+      <span className="car-slider-title"> 판매 중인 매물 </span>
+      <div className="car-list-slider" 
+            onMouseDown={onMouseDown}
+            onMouseMove={isDrag ? onDragMove : undefined}
+            onMouseUp={onMouseUp}
+            onMouseLeave={onMouseUp}
+            ref={scrollRef}>
         {carList.map((car, idx) => (
           <div className="car-card-wrapper" key={idx}>
-            <CarCard {...car} onClick={onClick} />
+            <CarCard {...car} onClick={handleClick} />
           </div>
         ))}
       </div>
--- src/dealerdetail/components/DealerLocation.tsx
+++ src/dealerdetail/components/DealerLocation.tsx
@@ -8,7 +8,7 @@ interface DealerLocationProps {
 export default function DealerLocation({ address }: DealerLocationProps) {
   return (
     <div className="dealer-location">
-      <h2 className="location-title">딜러 위치</h2>
+      <p className="location-title">딜러 위치</p>
       <p className="location-address">{address}</p>
     </div>
   );
