568d17e6dd3067ab8df8099559a3bb0fbf5ff5cd
mna11 <thkim306@gmail.com>
2025-06-22 21:28:45 +0900

    Refactor: Main 병합을 위한 폴더 구조 변경

--- src/main/java/net/likelion/picar/repository/DealerRepository.java
+++
@@ -1,9 +0,0 @@
-package net.likelion.picar.repository;
-
-import net.likelion.picar.domain.Dealer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DealerRepository extends JpaRepository<Dealer, Long> {
-}
